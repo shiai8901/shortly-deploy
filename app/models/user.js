ee97d83a6d229871cf7c7718d8e7650028d5fb52
Ai Shi <aishi@Ais-MBP-2.hsd1.ca.comcast.net>
2017-01-31 20:19:08 -0800

    refactored with mongoose

--- app/models/user.js
+++ app/models/user.js
@@ -1,25 +1,47 @@
 var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
+var mongoose = require('mongoose');
 
-var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function() {
-    this.on('creating', this.hashPassword);
-  },
-  comparePassword: function(attemptedPassword, callback) {
-    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-      callback(isMatch);
-    });
-  },
-  hashPassword: function() {
-    var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
-      .then(function(hash) {
-        this.set('password', hash);
-      });
-  }
+var userSchema = mongoose.Schema({
+  username: {type: String, index: {unique: true}},
+  password: String
 });
+
+var User = mongoose.model('User', userSchema);
+
+User.property.comparePassword = function(attemptedPassword, callback) {
+  bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
+    callback(isMatch);
+  });
+},
+
+userSchema.pre('save', function() {
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      this.password = hash;
+    });
+}
+
+// var User = db.Model.extend({
+//   tableName: 'users',
+//   hasTimestamps: true,
+//   initialize: function() {
+//     this.on('creating', this.hashPassword);
+//   },
+//   comparePassword: function(attemptedPassword, callback) {
+//     bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
+//       callback(isMatch);
+//     });
+//   },
+//   hashPassword: function() {
+//     var cipher = Promise.promisify(bcrypt.hash);
+//     return cipher(this.get('password'), null, null).bind(this)
+//       .then(function(hash) {
+//         this.set('password', hash);
+//       });
+//   }
+// });
 
 module.exports = User;
